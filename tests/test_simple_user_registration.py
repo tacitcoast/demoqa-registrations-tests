e8b2802f911eb446a7a26842af4773433b886760
Anna Malinovskaia <tacitcoast@gmail.com>
2023-07-27 22:47:30 +0100

    update simple test

--- tests/test_simple_user_registration.py
+++ tests/test_simple_user_registration.py
@@ -1,8 +1,14 @@
+import allure
 from demoqa.data.users import User2
 from demoqa.pages.application import app
 from demoqa.pages.simple_user_registration_page import SimpleUserRegistrationPage
 
-
+@allure.title('Successful fill simple registration form')
+@allure.tag("WEB")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Anna Malinovskaia")
+@allure.feature("Регистрационная короткая форма студента")
+@allure.story("Пользователь может зарегестрироваться")
 def test_registration_user():
     # GIVEN
     anna = User2(full_name='Anna Malinovskaia',
@@ -10,10 +16,13 @@ def test_registration_user():
                 current_address='Portugal, Lisbon',
                 permanent_address='Russia, Moscow')
 
-    app.simplified_registration.open()
+    with allure.step('Open registration form'):
+        app.simplified_registration.open()
 
     # WHEN
-    app.simplified_registration.register(anna)
+    with allure.step('Fill registration form'):
+        app.simplified_registration.register(anna)
 
     # THEN
-    app.simplified_registration.should_have_registred(anna)
+    with allure.step('Check results'):
+        app.simplified_registration.should_have_registred(anna)
