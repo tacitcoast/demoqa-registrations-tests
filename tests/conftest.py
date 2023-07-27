6769f2bbf3cad87f1cd12f977f6e9eac293a3a29
Anna Malinovskaia <tacitcoast@gmail.com>
2023-07-27 21:50:02 +0100

    add  @pytest.fixture(scope='function', autouse=True)

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session', autouse=True)
 def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
