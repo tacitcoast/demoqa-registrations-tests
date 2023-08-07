c02cfae5cc251e213cf3d5445823ed75909f87ee
Anna Malinovskaia <tacitcoast@gmail.com>
2023-08-07 19:06:39 +0100

    add 2.0.0rc3post3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options.binary_location = (
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    )
+
     yield
     browser.quit()
